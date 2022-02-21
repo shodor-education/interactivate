c4fce4dcd62f636192c8fe959b885dd56e4454d0
Aaron Weeden <aweeden@shodor.org>
2022-02-20 19:09:32 -0500

    Add favicon, fix styling, try out Basic Spinner activity code

---
+++ js/activities/BasicSpinner/BasicSpinner.parameters.js
@@ -0,0 +1,5 @@
+var parameters = 
+  "width=1000\n" +
+  "height=522\n" +
+  "isAdjustable=false\n" +
+  "num_sects=4";
---
+++ js/activities/copyrightYear.js
@@ -0,0 +1,7 @@
+---
+---
+/*
+  This is the maximum copyright year shown in the alert message that appears
+  when the user clicks on the "(c) Shodor" button.
+ */
+var currentCopyrightYear = "{{ 'now' | date: '%Y' }}";
