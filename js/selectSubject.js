971354072de09f4f9ed4e66c989afbf670ba87f5
Aaron Weeden <aweeden@shodor.org>
2022-02-21 19:06:03 -0500

    Add tab switcher, rename JS file to match function name

--- js/selectSubject.js
+++ js/selectSubject.js
@@ -1,9 +1,9 @@
 function selectSubject(name) {
   var sections = document.getElementsByClassName("subject-section");
   for (var i = 0; i < sections.length; i++) {
-    sections[i].style.display =
-      (name == "all" || sections[i].id == "subject-section-" + name) ?
-      "block" :
-      "none";
+    sections[i].style.display
+      = (name == "all" || sections[i].id == "subject-section-" + name)
+      ? "block"
+      : "none";
   }
 }
