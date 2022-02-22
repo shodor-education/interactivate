ac81872cec93506ed6d928b520db52866cd8ef13
Aaron Weeden <aweeden@shodor.org>
2022-02-22 15:49:12 -0500

    Rename img dir, exclude dir from generated site
    Rename 'images' to 'img' to save bytes, rename 'docs' to 'exclude' and mark it to be excluded from the generated site

--- js/selectTab.js
+++ js/selectTab.js
@@ -1,13 +1,13 @@
 ---
 ---
 function selectTab(sectionId) {
-  var imagesDir = "{{ 'images/activity/' | relative_url }}";
+  var imgDir = "{{ 'img/activity/' | relative_url }}";
   var sections = document.getElementsByClassName("tab-section");
   for (var i = 0; i < sections.length; i++) {
     var isSelected = (sections[i].id == sectionId);
     sections[i].style.display = isSelected ? "block" : "none";
     var tabName = sections[i].id.replace("-section", "");
     document.getElementById(tabName + "-img").src
-      = imagesDir + tabName + (isSelected ? "-selected" : "" ) + ".gif";
+      = imgDir + tabName + (isSelected ? "-selected" : "" ) + ".gif";
   }
 }
