4e392b43d1cf559fa57989b5d79e01b7408ad1c5
Aaron Weeden <aweeden@shodor.org>
2022-02-24 14:10:00 -0500

    Add audience catalog front matter and fix nav bug

--- js/selectCatalogCategory.js
+++ js/selectCatalogCategory.js
@@ -2,7 +2,7 @@ function selectCatalogCategory(categoryName) {
   var sections = document.getElementsByClassName("category-section");
   for (var i = 0; i < sections.length; i++) {
     sections[i].style.display
-      = (item == "all" || sections[i].id == "category-section-" + categoryName)
+      = (categoryName == "all" || sections[i].id == "category-section-" + categoryName)
       ? "block"
       : "none";
   }
