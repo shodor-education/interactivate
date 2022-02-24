ef486bbc741595d998d4565e2e6252ceee498d9d
Aaron Weeden <aweeden@shodor.org>
2022-02-24 13:59:24 -0500

    Rename list-pages to catalogs
    and add initial pages for catalogs sorted by subject and audience

--- js/selectCategoryItem.js
+++
@@ -1,9 +0,0 @@
-function selectCategoryItem(categoryLabel, item) {
-  var sections = document.getElementsByClassName(categoryLabel + "-section");
-  for (var i = 0; i < sections.length; i++) {
-    sections[i].style.display
-      = (item == "all" || sections[i].id == categoryLabel + "-section-" + item)
-      ? "block"
-      : "none";
-  }
-}
