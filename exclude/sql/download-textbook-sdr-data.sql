b73fa977c449880ba41041fb07ad1d16421abd63
Aaron Weeden <aweeden@shodor.org>
2022-03-11 10:30:13 -0500

    Split textbook title into series and book

--- exclude/sql/download-textbook-sdr-data.sql
+++ exclude/sql/download-textbook-sdr-data.sql
@@ -1,9 +1,11 @@
 -- Download textbook SDR data
 select concat(
   "FILENAME::", bookId, "\r---\r",
+  "book: \"", gradeOrCourse, "\"\r",
   "chapters:\r- \"",
   group_concat(TSDChapter.`name` separator "\"\r- \""),
   "\"\r",
+  "series: \"", title, "\"\r",
   "title: \"", title, " ", gradeOrCourse, "\"\r",
   "---"
 )
